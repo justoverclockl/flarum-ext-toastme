b6b2bf37ecef43967ce2da48f1670be2c0deb173
Justoverclock <info@justoverclock.it>
2021-05-04 00:33:20 +0200

    Unstable Beta

--- js/dist/forum.js
+++ js/dist/forum.js
@@ -569,25 +569,33 @@ __webpack_require__.r(__webpack_exports__);
 
 
 flarum_app__WEBPACK_IMPORTED_MODULE_2___default.a.initializers.add('justoverclock/flarum-ext-toastme', function () {
-  toastify_js__WEBPACK_IMPORTED_MODULE_0___default()({
-    text: "This is a toast",
-    duration: -1,
-    destination: "https://flarum.it",
-    newWindow: true,
-    close: true,
-    gravity: "top",
-    // `top` or `bottom`
-    position: "right",
-    // `left`, `center` or `right`
-    backgroundColor: "#e39709",
-    stopOnFocus: true,
-    // Prevents dismissing of toast on hover
-    onClick: function onClick() {} // Callback after click
+  Object(flarum_common_extend__WEBPACK_IMPORTED_MODULE_1__["extend"])(flarum_components_IndexPage__WEBPACK_IMPORTED_MODULE_3___default.a.prototype, 'view', function (vdom) {
+    if (flarum_app__WEBPACK_IMPORTED_MODULE_2___default.a.forum.attribute('EnableToastify') === true) if (vdom.children && vdom.children.splice) {
+      var insert = toastify_js__WEBPACK_IMPORTED_MODULE_0___default()({
+        text: flarum_app__WEBPACK_IMPORTED_MODULE_2___default.a.translator.trans('flarum-ext-toastme.forum.message1'),
+        duration: -1,
+        offset: {
+          x: 50,
+          // horizontal axis - can be a number or a string indicating unity. eg: '2em'
+          y: 400 // vertical axis - can be a number or a string indicating unity. eg: '2em'
 
-  }).showToast();
+        },
+        destination: "https://flarum.it",
+        newWindow: true,
+        close: true,
+        gravity: "top",
+        // `top` or `bottom`
+        position: "right",
+        // `left`, `center` or `right`
+        backgroundColor: "#e39709",
+        stopOnFocus: true // Prevents dismissing of toast on hover
+
+      }).showToast();
+      if (insert) return;
+      vdom.children.splice(1, 0, insert);
+    }
+  });
 });
-/*
-if (app.forum.attribute('EnableToastify') === true)*/
 
 /***/ }),
 
--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -3,25 +3,30 @@ import { extend } from 'flarum/common/extend';
 import app from 'flarum/app';
 import IndexPage from 'flarum/components/IndexPage';
 
-
 app.initializers.add('justoverclock/flarum-ext-toastme', () => {
+  extend(IndexPage.prototype, 'view', function (vdom) {
+    if (app.forum.attribute('EnableToastify') === true)
+      if (vdom.children && vdom.children.splice) {
+        const insert = Toastify({
+          text: app.translator.trans('flarum-ext-toastme.forum.message1'),
+          duration: -1,
+          offset: {
+            x: 50, // horizontal axis - can be a number or a string indicating unity. eg: '2em'
+            y: 400 // vertical axis - can be a number or a string indicating unity. eg: '2em'
+          },
+          destination: "https://flarum.it",
+          newWindow: true,
+          close: true,
+          gravity: "top", // `top` or `bottom`
+          position: "right", // `left`, `center` or `right`
+          backgroundColor: "#e39709",
+          stopOnFocus: true, // Prevents dismissing of toast on hover
+        }).showToast();
+        if(insert) return;
+        vdom.children.splice(1, 0, insert);
 
-  Toastify({
-    text: "This is a toast",
-    duration: -1,
-    destination: "https://flarum.it",
-    newWindow: true,
-    close: true,
-    gravity: "top", // `top` or `bottom`
-    position: "right", // `left`, `center` or `right`
-    backgroundColor: "#e39709",
-    stopOnFocus: true, // Prevents dismissing of toast on hover
-    onClick: function(){} // Callback after click
-  }).showToast();
+      }
+  })
 })
-
-
-/*
-if (app.forum.attribute('EnableToastify') === true)*/
 
 
